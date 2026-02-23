bb48065b830952341cf9870a4276a793b7020e7a
Mike Kostov <mike.kostov@gmail.com>
2026-02-23 20:59:31 +0200

    Add ecto_sql dependency

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule PhxMediaLibrary.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A robust, Ecto-backed media management library for Elixir and Phoenix.",
+      package: package()
     ]
   end
 
@@ -21,8 +23,17 @@ defmodule PhxMediaLibrary.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ecto_sql, "~> 3.13.4"},
+      {:postgrex, ">= 0.0.0", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mike-kostov/phx_media_library"}
     ]
   end
 end
